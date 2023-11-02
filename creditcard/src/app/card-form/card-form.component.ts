36a00e9b3bfacdacfc57f247de883a8be2ab9cd2
Simone Ramundi <simone.ramundi@4science.com>
2023-11-02 12:27:06 +0100

    Adding styles

--- creditcard/src/app/card-form/card-form.component.ts
+++ creditcard/src/app/card-form/card-form.component.ts
@@ -11,9 +11,9 @@ export class CardFormComponent implements OnInit {
     name: new FormControl('',[
       Validators.required,
       Validators.minLength(3),
-      Validators.maxLength(5),
-      Validators.pattern(/\s/)
     ]),
+
+
 
   });
 
--- creditcard/src/app/input/input.component.ts
+++ creditcard/src/app/input/input.component.ts
@@ -8,7 +8,13 @@ import { AbstractControl, FormControl, FormGroup } from "@angular/forms";
 })
 export class InputComponent implements OnInit {
   @Input() control: FormControl;
+  @Input() label: string;
   constructor() { }
+  showErrors(){
+    const { dirty, touched, errors} = this.control;
+    return dirty && touched && errors
+  }
+
   ngOnInit(): void {
   }
 
