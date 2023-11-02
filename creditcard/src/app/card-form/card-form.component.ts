69a2b58fd059dfbd30350039efcf0ace4a5e6cc9
Simone Ramundi <simone.ramundi@4science.com>
2023-11-02 12:58:43 +0100

    Adding additional validators

--- creditcard/src/app/card-form/card-form.component.ts
+++ creditcard/src/app/card-form/card-form.component.ts
@@ -8,13 +8,10 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 })
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
-    name: new FormControl('',[
-      Validators.required,
-      Validators.minLength(3),
-    ]),
-    cardNumber: new FormControl(''),
-    expiration: new FormControl(''),
-    securityCode: new FormControl(''),
+    name: new FormControl('',[Validators.required, Validators.minLength(3),]),
+    cardNumber: new FormControl('',[Validators.required, Validators.minLength(16), Validators.maxLength(16)],),
+    expiration: new FormControl('',[Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]),
+    securityCode: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]),
   });
 
   constructor() {
